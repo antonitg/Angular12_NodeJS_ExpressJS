a543ae4f3ee022cbb260d086cbf9ba55753657aa
Antoni Tormo Garcia <gineraantoni@gmail.com>
2021-09-22 17:41:47 +0200

    Arreglat routing de categories en backend

--- client/src/app/product/product-create.component.ts
+++ client/src/app/product/product-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Product } from './models/productModel';
 
 @Component({
@@ -11,9 +11,37 @@ export class ProductCreateComponent implements OnInit {
     constructor(
       private route: ActivatedRoute,
       private router: Router,
-      private cd: ChangeDetectorRef
-    ) {}
-    
+      private cd: ChangeDetectorRef,
+      private prodToCreate: FormGroup,
+      private fb: FormBuilder
+      ) {
+        this.prodToCreate = this.fb.group({
+          id: [''],
+          nom: ['', Validators.required],
+          categoria:['', Validators.required],
+          descr:['', Validators.maxLength(1000)],
+          preu:['', Validators.required],
+          id_bar:['', Validators.required],
+  
+        })
+     }
+
+  // descr: {
+  //     type: String,
+  //     required: true,
+  // },
+  // id_cat: {
+  //     type: String,
+  //     required: true,
+  // },
+  // preu: {
+  //     type: Number,
+  //     required: true,
+  // },
+  // stock: {
+  //     type: Boolean,
+  //     required: true,
+  // }
     ngOnInit() {
       console.log("asdasdzz");
     }
