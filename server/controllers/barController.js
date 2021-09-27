919ac496d3fd11b615627ad6cae36becd58633b7
Juan Antonio López Seguí <juan.antonio.lis@gmail.com>
2021-09-27 12:02:21 +0200

    mejorando la creacion de bares. Se puede agregar el bar a 2 categorias

--- server/controllers/barController.js
+++ server/controllers/barController.js
@@ -1,34 +1,64 @@
 const { op } = require('sequelize');
 const Bar = require('../models').bar;
+const BarCatego = require('../models').bar_catego_in;
 const { generateId } = require('../utils/generateIds');
 
-module.exports = {
-    createBar(req, res) {
-        return Bar.create({
-                id: generateId(),
-                nom: req.body.nom,
-                descr: req.body.descr,
-                direcc: req.body.descr,
-                city: req.body.city,
-                coords: req.body.coords,
-                horari: req.body.horari,
-                owner: req.body.owner,
-                createdAt: new Date(),
-                updatedAt: new Date(),
-            }).then(bar => res.status(200).send(bar))
-            .catch(error => res.status(400).send(error));
-    },
-    getBars(req, res) {
-        return Bar.findAll({
-            where: {
-                [op.like]: req.params.id_catego,
-            }
+module.exports.createBar = async(req, res) => {
+    try {
+        var c_bar = await Bar.create({
+            id: generateId(),
+            nom: req.body.nom,
+            descr: req.body.descr,
+            direcc: req.body.direcc,
+            city: req.body.city,
+            coords: req.body.coords,
+            horari: req.body.horari,
+            owner: req.body.owner,
+            createdAt: new Date(),
+            updatedAt: new Date(),
         });
-    },
-    updateBarInfo(req, res) {
 
-    },
-    deleteBar(req, res) {
+        if (req.body.id_catego.length == 2) {
+            for (let i = 0; i < req.body.id_catego.length; i++) {
+                await BarCatego.create({
+                    id_catego: req.body.id_catego[i],
+                    id_bar: c_bar.id,
+                });
+            }
+        } else if (req.body.id_catego.length == 1) {
+            await BarCatego.create({
+                id_catego: req.body.id_catego[0],
+                id_bar: c_bar.id,
+            });
+        }
 
+        res.json(c_bar);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('ERROR 500');
+    }
+}
+module.exports.getBars = async(req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('ERROR 500');
+    }
+}
+module.exports.updateBarInfo = async(req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('ERROR 500');
+    }
+}
+module.exports.deleteBar = async(req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('ERROR 500');
     }
 }
