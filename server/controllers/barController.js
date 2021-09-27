d18ade3d73a6aff58c785a5c911b54438ab901c7
Juan Antonio López Seguí <juan.antonio.lis@gmail.com>
2021-09-27 19:15:27 +0200

    solucionando bug categorias de bares

--- server/controllers/barController.js
+++ server/controllers/barController.js
@@ -59,8 +59,44 @@ module.exports.getBars = async(req, res) => {
             limit = 10;
         }
 
-        var g_bars = await db.sequelize.query('SELECT b.*, bc.nom as catego, bc.id as id_catego FROM bars b, bar_categos bc, bar_catego_ins bci WHERE b.id = bci.id_bar AND bci.id_catego = bc.id AND b.nom LIKE "%' + search + '%" AND b.city LIKE "%' + city + '%" AND bc.nom LIKE "%' + catego + '%" LIMIT ' + limit, { type: QueryTypes.SELECT });
-        res.json(g_bars);
+        var g_bars = await db.sequelize.query('SELECT b.*, bc.nom as catego FROM bars b, bar_categos bc, bar_catego_ins bci WHERE b.id = bci.id_bar AND bci.id_catego = bc.id AND b.nom LIKE "%' + search + '%" AND b.city LIKE "%' + city + '%" AND bc.nom LIKE "%' + catego + '%" ORDER BY b.id LIMIT ' + limit, { type: QueryTypes.SELECT });
+
+
+        if (g_bars.length > 0) {
+            var bars = new Array({
+                id: g_bars[0].id,
+                nom: g_bars[0].nom,
+                slug: g_bars[0].slug,
+                descr: g_bars[0].descr,
+                direcc: g_bars[0].direcc,
+                city: g_bars[0].city,
+                coords: g_bars[0].coords,
+                horari: g_bars[0].horari,
+                owner: g_bars[0].owner,
+                catego: [g_bars[0].catego]
+            });
+            for (let i = 1; i < g_bars.length; i++) {
+                if (bars[bars.length - 1].id == g_bars[i].id) {
+                    bars[bars.length - 1].catego.push(g_bars[i].catego);
+                } else {
+
+                    bars.push({
+                        id: g_bars[i].id,
+                        nom: g_bars[i].nom,
+                        slug: g_bars[i].slug,
+                        descr: g_bars[i].descr,
+                        direcc: g_bars[i].direcc,
+                        city: g_bars[i].city,
+                        coords: g_bars[i].coords,
+                        horari: g_bars[i].horari,
+                        owner: g_bars[i].owner,
+                        catego: [g_bars[i].catego]
+                    });
+                }
+            }
+        }
+
+        res.json(bars);
     } catch (error) {
         console.log(error);
         res.status(500).send('ERROR 500');
