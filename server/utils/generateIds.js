0e05f3c3a3ebc4cd73d9ecea356d1138c582ee98
Juan Antonio López Seguí <juan.antonio.lis@gmail.com>
2021-09-28 12:08:13 +0200

    secret id

--- server/utils/generateIds.js
+++ server/utils/generateIds.js
@@ -1,11 +1,11 @@
 const sha256 = require('crypto-js/sha256');
 const hmacSHA512 = require('crypto-js/hmac-sha512');
-const base64 = require('crypto-js/enc-base64');
+require('dotenv').config({ path: 'config/variables.env' });
 
 module.exports.generateId = () => {
     var datetime = new Date();
     var id = datetime.getTime() + (Math.random() * Math.pow(36, 6) | 0).toString(36);
     var hashDigest = sha256(id);
-    var myid = hmacSHA512(hashDigest, "mySecret").toString();
+    var myid = hmacSHA512(hashDigest, process.env.SECRET_ID).toString();
     return myid;
 }
