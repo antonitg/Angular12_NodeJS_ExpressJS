f7819a012828d25b87cb83e2d75cad1b47c868e4
Antoni Tormo Garcia <gineraantoni@gmail.com>
2021-10-18 11:07:31 +0200

    Register validations

--- client/src/app/auth/auth.component.ts
+++ client/src/app/auth/auth.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormControlName } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormControlName, ValidationErrors } from '@angular/forms';
 import { left, right } from '@popperjs/core';
 import { UserService } from '../core/services/user.service';
 
@@ -8,41 +8,44 @@ import { UserService } from '../core/services/user.service';
   templateUrl: './auth.component.html',
 })
 
-export class AuthComponent implements OnInit, OnChanges {
+export class AuthComponent implements OnInit {
   @ViewChild('singIn') btSignIn!: ElementRef;
   @ViewChild('singUp') btSignUp!: ElementRef;
   @ViewChild('logcontainer') container!: ElementRef;
-
-  validPassword = false
+  validPassword = 0
+  validEmail = 0
+  strongPassword = 0
   user!: {};
 
   constructor(
     private UserService : UserService
   ) {  }
 
-
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"
+  passPattern = '((?=.*)(?=.*[a-z])(?=.*[A-Z]).{8,30})'
   regForm = new FormGroup({
     nom: new FormControl(null, Validators.required),
     passwd: new FormControl(null,Validators.required),
     repasswd: new FormControl(null,Validators.required),
     email: new FormControl(null,Validators.required),
 
-
   })
 
   ngOnInit(): void {
     console.log(this.container);
   }
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes);
-
-    this.checkPass()
-    console.log(this.validPassword);
+  checkPass():void {
+    this.validPassword = this.regForm.value["passwd"] == this.regForm.value["repasswd"]? 2 : 1
+    this.strongPassword = this.regForm.get("passwd")?.status == "VALID"? 2 : 1
+    console.log(this.regForm.get("passwd"));
 
 
   }
-  checkPass():void {
-    this.validPassword = this.regForm.value["regPass"] == this.regForm.value["regPass2"]? true : false
+  checkEmail():void {
+    this.validEmail = this.regForm.get("email")?.status == "VALID"? 2 : 1
+    // console.log(this.regForm.get("email")?.status);
+    console.log(this.validEmail, this.validPassword, this.strongPassword);
+
 
   }
   rightPanel() {
